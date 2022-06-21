7e6e1290905595116477c381a44f063b34fb7e61
snowjac <107868306+snowjac@users.noreply.github.com>
2022-06-21 16:42:30 +0100

    Update streamlit_app.py
    Add fruit_macros dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -7,3 +8,6 @@ streamlit.text('🥬 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🥚 Soft-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado on Toast')
 streamlit.header('🍌🥭 Build your own Smoothie 🫐🥝')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
