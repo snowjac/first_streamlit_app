955cb64586746d7ad58a19c476798f5e6a2694ba
snowjac <107868306+snowjac@users.noreply.github.com>
2022-06-21 18:04:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,23 @@ import streamlit
 import pandas
 import numpy
 
-#streamlit.title('My Parents New Healthy Diner')
-#streamlit.header('Breakfast Menu')
-#streamlit.text('🥣 Omega3 and Blueberry Muesli')
-#streamlit.text('🥬 Kale, Spinach and Rocket Smoothie')
-#streamlit.text('🥚 Soft-Boiled Free-Range Egg')
-#streamlit.text('🥑🍞 Avocado on Toast')
-#streamlit.header('🍌🥭 Build your own Smoothie 🫐🥝')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega3 and Blueberry Muesli')
+streamlit.text('🥬 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🥚 Soft-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado on Toast')
+streamlit.header('🍌🥭 Build your own Smoothie 🫐🥝')
 
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list = my_fruit_list.set_index('Fruit')
-#fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Strawberries'])
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-#streamlit.dataframe(fruits_to_show)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 df = pandas.DataFrame(
-     numpy.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+     numpy.random.randn(1000, 2) / [50, 50] + [52.109, -1.316],
      columns=['lat', 'lon'])
 
 streamlit.map(df)
