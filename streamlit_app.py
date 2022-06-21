600496cadb2dc39ca51ceb2550881c2ce1510554
snowjac <107868306+snowjac@users.noreply.github.com>
2022-06-21 16:18:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry Muesli')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Soft-Boiled Free-Range Egg')
